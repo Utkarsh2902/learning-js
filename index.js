8201cac56f5164df55bb3bdc9aafba8b72c80f4e
Utkarsh Gupta <69791796+Utkarsh2902@users.noreply.github.com>
2023-04-11 15:47:53 +0000

    input process output example

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-var readlineSync=require("readline-sync"); 
-console.log("my name is Utkarsh gupta");
+ var readlineSync=require("readline-sync"); 
+// console.log("my name is Utkarsh gupta");
+var userName=readlineSync.question('what is your name?'); console.log(userName);
+//var userFriend=readlineSync.question('what is your friends name');
+var score=0;
+var details="hey"+userName;
+console.log(details);
+if(userName=="Utkarsh")
+{
+  score++;
+}
+else
+{
+  score--;
+}
+console.log(score);
