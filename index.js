f7d072671293231d0c4a4b1d2d9d4050b313eee6
Utkarsh Gupta <69791796+Utkarsh2902@users.noreply.github.com>
2023-04-14 07:08:54 +0000

    basic conditions

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
  var readlineSync=require("readline-sync"); 
 // console.log("my name is Utkarsh gupta");
-var userName=readlineSync.question('what is your name?'); console.log(userName);
-//var userFriend=readlineSync.question('what is your friends name');
 var score=0;
-var details="hey"+userName;
-console.log(details);
-if(userName=="Utkarsh")
+var userAge=readlineSync.question('what is your age?\n');
+var userAdd=readlineSync.question('where do you live?\n');
+if(userAge>=25)
 {
-  score++;
+  if(userAdd=="bsr")
+  {
+   score++;
+  }
 }
 else
 {
   score--;
 }
 console.log(score);
+
+//var userFriend=readlineSync.question('what is your friends name');
+// var details="hey"+userName;
+// console.log(details);
+// if(userName=="Utkarsh")
+// {
+//   score++;
+// }
+// else
+// {
+//   score--;
+// }
+// console.log(score);
