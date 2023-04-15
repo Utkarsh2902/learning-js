528f3f18d21192bf764bd8d9b837ee7c91a0b49b
Utkarsh Gupta <69791796+Utkarsh2902@users.noreply.github.com>
2023-04-15 18:55:11 +0000

    loop inside function

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 var readlineSync=require("readline-sync");
-var score=0;
 var ques;
-var ans;
-function ask(ques,ans)
+function fun1(ans)
 {
-  var userAsk=readlineSync.question(ques);
-  
-  if(userAsk==ans)
-  {
-    score++;
-  }
-  else
-  {
-    score--;
-  }
-  return score;
+  var ques=readlineSync.question('what is my name\n');
+return ques
 }
-console.log(ask("what is my current stipend\n",10000));
-// var score=0;
-// var num1;
-// var num2;
-// var num3;
-// function fun1(num1,num2)
-// {
-//   num3=num1+num2;
-//   return num3;
-// }
-// console.log(fun1(1,3));
-// //or
-// var result=fun1(1,3);
-// console.log(result);
+for(var i=0;i<5;i++)
+  {
+    console.log(fun1("Utkarsh"));
+  }
