ec3aff82d53038ff69180140be1bb213a6b5d31a
Utkarsh2902 <69791796+Utkarsh2902@users.noreply.github.com>
2023-04-20 14:55:18 +0000

    function using key:values

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var readlineSync=require("readline-sync");
 // var N=readlineSync.question('Please enter the number')
-
+var user1=readlineSync.question('choose your hero\n')
 var superman={
 color: "red",
   design: "funny",
@@ -12,8 +12,22 @@ color: "red",
     design: "cool",
     strength: "35%"
   };
-console.log(superman.strength);
-console.log(batman.strength);
-console.log(superman.strength>batman.strength?superman.color:batman.color);
+// console.log(superman.strength);
+// console.log(batman.strength);
+// console.log(superman.strength>batman.strength?superman.color:batman.color);
+
+function myHero(user)
+{
+  if(user1==batman)
+  {
+    console.log(batman.design)
+  }
+  else
+  {
+    console.log(superman.design)
+  }
+}
+myHero(batman);
+
 
 
