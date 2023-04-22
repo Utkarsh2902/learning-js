06313e0a1054bab9ddae2c7683f772d1897195e8
Utkarsh2902 <69791796+Utkarsh2902@users.noreply.github.com>
2023-04-22 14:54:01 +0000

    Mark1 How much you know

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
 
-var readlineSync=require("readline-sync");
-// var N=readlineSync.question('Please enter the number')
-var user1=readlineSync.question('choose your hero\n')
-var superman={
-color: "red",
-  design: "funny",
-  strength: "50%"
-};
-  var batman={
-    color: "black",
-    design: "cool",
-    strength: "35%"
-  };
-// console.log(superman.strength);
-// console.log(batman.strength);
-// console.log(superman.strength>batman.strength?superman.color:batman.color);
-
-function myHero(user)
+ var readlineSync=require("readline-sync");
+ var score=0;
+function invite()
 {
-  if(user1==batman)
-  {
-    console.log(batman.design)
-  }
-  else
-  {
-    console.log(superman.design)
-  }
+var userName=readlineSync.question('What is your name?\n')
+console.log("Hey "+userName)
+console.log("Lets play a game to check how much you know about INDIA\n")
 }
-myHero(batman);
-
-
-
+var quiz=[{
+  question:"Which India city is also called the pink city?\n",
+    answer:"Jaipur",
+          },
+          {
+  question: "Which state has the largest area?\n",
+    answer:"Rajasthan",
+          },
+          { 
+  question: "National bird of India?\n",
+    answer: "Peacock",
+          },
+          {
+  question: "Which city is closet to the geographical center of India?\n",
+    answer: "Nagpur",
+          },
+          {
+  question:"Which state has the largest population?\n",
+    answer:  "UttarPradesh",
+          }
+];
+function results(question,answer)
+{
+  var userAns=readlineSync.question(question)
+      if(userAns.toUpperCase()==answer.toUpperCase())
+      {
+        console.log("Nice!")
+        score++;
+        console.log("Score->"+ score)
+      }
+  else
+      {
+        console.log("Oops")
+        score--;
+        console.log("score->"+ score)
+      }
+}
+function game(){
+  for(var i=0;i<quiz.length;i++)
+    {
+      var current=quiz[i];
+      results(current.question,current.answer);
+    }
+ showResults();
+}
+function showResults()
+{
+    console.log("---------------\n---------------\n");
+   console.log("Your finall score is\n"+score);
+   console.log("You can also check the score list");
+}
+invite();
+game();
+results();
