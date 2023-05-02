66656ee693190b653f62f4597fa52afe7fe7201e
Utkarsh2902 <69791796+Utkarsh2902@users.noreply.github.com>
2023-05-02 20:25:21 +0000

    Mark1 How much you know

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
-var readlineSync = require("readline-sync");
-var score = 0;
-function welcomeUser()
+
+ var readlineSync=require("readline-sync");
+ var score=0;
+function invite()
 {
 var userName=readlineSync.question('What is your name?\n')
 console.log("Hey "+userName)
-console.log("Lets play a game to check how much you know about Harry Potter Series\n")
- 
+console.log("Lets play a game to check how much you know about INDIA\n")
 }
-var list = [{
-  question: "Real name of Harry Potter\n",
-  answer: "Daniel Radcliffe",
-},
-{
-  question: "Sister of Ronald Weasley?\n",
-  answer: "Ginny Weasley",
-},
-{
-  question: "How many series are made upon Harry Potter?\n",
-  answer: "8",
-},
-{
-  question: "At the time of Hogwarts admission Age of Harry is ?\n",
-  answer: "11",
-},
-{
-  question: "In Quidditch game harry played as\n",
-  answer: "Seeker",
-}
+var quiz=[{
+  question:"Which India city is also called the pink city?\n",
+    answer:"Jaipur",
+          },
+          {
+  question: "Which state has the largest area?\n",
+    answer:"Rajasthan",
+          },
+          { 
+  question: "National bird of India?\n",
+    answer: "Peacock",
+          },
+          {
+  question: "Which city is closet to the geographical center of India?\n",
+    answer: "Nagpur",
+          },
+          {
+  question:"Which state has the largest population?\n",
+    answer:  "UttarPradesh",
+          }
 ];
-function check(question, answer) {
-  var userAns = readlineSync.question(question)
-  if (userAns.toUpperCase() == answer.toUpperCase()) {
-    console.log("Right!")
-    score++;
-    console.log("Score->" + score)
-  }
-  else {
-    console.log("Wrong!")
-    // score--;
-    console.log("score->" + score)
-  }
+function results(question,answer)
+{
+  var userAns=readlineSync.question(question)
+      if(userAns.toUpperCase()==answer.toUpperCase())
+      {
+        console.log("Nice!")
+        score++;
+        console.log("Score->"+ score)
+      }
+  else
+      {
+        console.log("Oops")
+        score--;
+        console.log("score->"+ score)
+      }
 }
-function results() {
-  for (var i = 0; i < list.length; i++) {
-    var quizQuestion = list[i];
-    check(quizQuestion.question, quizQuestion.answer)
-  }
-  scores();
+function game(){
+  for(var i=0;i<quiz.length;i++)
+    {
+      var current=quiz[i];
+      results(current.question,current.answer);
+    }
+ showResults();
 }
-function scores() {
-  console.log("---------------\n---------------\n");
-  console.log("Your finall score is\n" + score);
-  console.log("You can also check the score list");
+function showResults()
+{
+    console.log("---------------\n---------------\n");
+   console.log("Your finall score is\n"+score);
+   console.log("You can also check the score list");
 }
-welcomeUser();
+invite();
+game();
 results();
-check();
